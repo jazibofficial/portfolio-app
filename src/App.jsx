98718aadd7b4ebf98ac37a215d8acc48c26f9477
Muhammad jazib ali <jazibalisher@gmail.com>
2023-10-17 07:06:04 -0700

    3update

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <div class="text-center">
+  <img src="jazib.jpeg" class="rounded-circle" alt="my pic" height={300} width={300}  />
+</div>
+  
+  <h1>JazibTech</h1>
+
+  <p>MernStack web Developer <br></br> 
+  <i class="fa fa-id-card" aria-hidden="true"></i>
+  <i class="fa fa-mobile" aria-hidden="true"></i>
+  </p>
+
+
+  <span class="border">
+    
+    
+     <h1>AbOUT ME</h1>
+
+     <p>My name is Jazib, I'm MERN Stack developer based in Lahore, punjab ,Pakistan. I have one year experience in developing things for websites. I like to craft solid and scalable Frontend & Backend products with great user experiences. I  am able to responsible for planning and developing software solutions and web applications, supporting and maintaining a company's websites and digital products. The day-to-day work of the web developer highly depends on constantly evolving internet innovations.</p>
+   </span>
+
+
+   <div class="row">
+  <div class="col-sm-6 mb-3 mb-sm-0">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"> Education</h5>
+        <p class="card-text"> Matric 10 years   <br /> FSC pre-Eng 12 years  <br /> Bs software-Engineering conntinue at UCP   Lahore  16 years   <br /> Courses  with different  institute  <br />   Arfa cream , Nexsubarry , infotech college  </p>
+        <a href="#" class="btn btn-primary"> primitives</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">EXPERINCE</h5>
+        <p class="card-text">1+Year  <br />  Fronted-Developer  <br /> Backend-Developer <br /> Full Stack-Developer  <br />Mern-stack Developer </p>
+        <a href="#" class="btn btn-primary"> Experties very soon</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+</div>
+
+
+
+  
+  );
+}
+
+export default App;
